c60e44a6e024c3596d20f07c3a55e9b98c21d2b2
Diane Hosfelt <dianehosfelt@gmail.com>
2015-10-08 19:33:44 +0100

    ruby version: need to refactor to properly score

---
+++ ratename.rb
@@ -0,0 +1,53 @@
+# Script to rank name sexiness
+
+#TODO write something to auto generate the name list
+#Current source: https://en.wikipedia.org/wiki/List_of_pornographic_actresses_by_decade
+
+#if ARGV.size < 1
+#    puts 'How can I tell you if your name is sexy if you don\'t give me one??'
+#    exit
+#end
+
+$alphabet = [*('a'..'z')]
+$pscounts = [0]*alphabet.size
+$firstnames = []
+
+
+#TODO this is kinda c-ish
+#sums character ocurrences and returns them in the same array it was passed
+def count_letters (name, counts)
+    $alphabet.each_with_index { |val, index| counts[index] += name.count(val) }
+    counts
+end
+
+#calculates the score for a given name
+def calc_score(name)
+    count = [0]*$alphabet.size
+    count_letters(name, count)
+
+    score = count.each_with_index {|val, idx| puts count[idx] *= $pscounts[idx]}.inject(:+)
+    puts "score: #{score}"
+    score = score / (name.size - name.count(' '))
+    score *= 2 if $firstnames.include? name.downcase.split(' ').first
+
+end
+
+#yourname = ARGV.first.downcase
+yourname = 'Diane Hosfelt'.downcase!
+
+psnames = File.readlines('psnamelist.txt').each {|l| l.chomp!.downcase!};
+psnames.each do |l| 
+    firstnames << l.split(' ').first
+end
+
+
+psnames.each { |name| count_letters(name, pscounts)}
+    
+yourscore = calc_score(yourname)
+
+
+
+
+#Use their name in a random number generator
+
+
